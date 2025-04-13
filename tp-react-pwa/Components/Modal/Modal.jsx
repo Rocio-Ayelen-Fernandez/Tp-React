a176c97adef2122cb72f16cc61ca20aeff682573
Rocio-Ayelen-Fernandez <rayelen.fernandez@est.fi.uncoma.edu.ar>
2025-04-13 19:33:52 -0300

    refactor Modal and Nav components to manage state and addmovie functionality

--- tp-react-pwa/Components/Modal/Modal.jsx
+++ tp-react-pwa/Components/Modal/Modal.jsx
@@ -1,11 +1,21 @@
+import Button from '../Button/Button'
 
-
-const Modal = (isOpen, onClose) =>{
+const Modal = ({isOpen, onClose, children}) =>{
+    
     let valReturn
     if (!isOpen){
         valReturn = null
     }else{
-        valReturn = <div>Hola</div>
+        valReturn = (
+            <div>
+                <Button name={"Cerrar"} onclick={() =>{
+                    console.log("cerrado")
+                    onClose()
+                }}/>
+                {children}
+            </div>
+        )
+        
     }   
 
     return valReturn
--- tp-react-pwa/pages/Home/Home.jsx
+++ tp-react-pwa/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import styles from './Home.module.css'
 import List from '../../Components/List/List'
 import Nav from '../../Components/Nav/Nav'
-// import {useState} from "react";
+import Button from '../../Components/Button/Button'
+import {useState} from "react";
+import Modal from '../../Components/Modal/Modal'
 
 
-const Home = () =>{
+const Home = () =>{ 
 
    /*
 
@@ -22,29 +24,78 @@ useEffect(() => {
 
 
    */
+    //Modal
+    const [showModal, setShowModal] = useState(false)
+    const [modalContent, setModalContent] = useState(null)
 
-    // const [listaPorVer, setListaPorVer] = useState([["serie1", "serie2"]]);
-    // const [listaVistas, setListaVistas] = useState([["serie3", "serie4"]]);
+    //Listas
+    const [listaPorVer, setListaPorVer] = useState([["serie1"], ["serie2"]])
+    const [listaVistas, setListaVistas] = useState([["serie3"], ["serie4"]])
 
-
-    // const addMovie = () => {
-    //     const newMovie = window.prompt("Ingrese el nombre de la pelicula: ")
+    
+    //Funciones
+    const addMovie = () => {
+        // const newMovie = window.prompt("Ingrese el nombre de la pelicula: ")
         
-    //     setListaPorVer([...listaPorVer, newMovie]);
+        // setListaPorVer([...listaPorVer, newMovie])
 
-    // }
+        //Almacena el texto ingresado
+        let movieName = ""
 
-    let listaPorVer = ["serie1", "serie2"];
-    let listaVistas = ["serie3", "serie4"];
+        
+        //Almacena el dato ingresado
+        const handleInput = (e) => {
+            movieName = e.target.value
+            // console.log(movieName)
+        }
 
+        //Verifica que no este vacio el input y actualiza la lista
+        //"cierra" el modal
+        const handleConfirm = () => {
+            if(movieName.trim() !== ""){
+                setListaPorVer([...listaPorVer, movieName])
+                console.log("enviado")
+                setShowModal(false)
+                setModalContent(null)
+            }
+        }
+
+        //Escribe los datos dentro del modal
+        const content = (
+            <div>
+                <h3>Agregar Pelicula</h3>
+                <input type="text" onChange={handleInput} />
+                <Button name={"Agregar"} onclick={handleConfirm}/>
+            </div>
+        )
+
+        setModalContent(content) //Este muestra el contenido
+        setShowModal(true) //Este muestra el Modal, lo hace "visible"
+        console.log("abierto")
+
+    }
+
+    const actions ={
+        //Listar Funciones aca
+        agregar: addMovie
+    }
+    const buttons = [
+        //Agregar Botones para el nav aca
+        {name: "Agregar", action: "agregar"},
+    ]
 
     return (
         <div className={styles.mainContainer}>
+            <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+                    {modalContent}
+            </Modal>
             <div className={styles.navContainer}>
-                <Nav/>
+                {/* Aca va el nav */}
+                <Nav actions={actions} buttons={buttons}/>
             </div>
             <div className={styles.mainGrid}>
-
+                
+                {/* Aca van las listas */}
                 <div className={styles.listContainer}>
                 
                 <List title={"Sin ver"} array={listaPorVer}/>
